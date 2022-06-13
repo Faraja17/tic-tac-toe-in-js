a56e8fb543ff8a4d54acf3d68487735f7b0b1149
Faraja17 <farajakthompson@gmail.com>
2022-06-13 14:44:16 -0400

    wrote players enter moves loop
    IT WORKS!
    
    It was tough translating this from Python due to syntax issues. The move prompt returns a string, so I couldn't just replace the key with it because in JS the key is not a string. but I researched a way to do it!

--- ticTacToe.js
+++ ticTacToe.js
@@ -8,32 +8,53 @@ const theBoard = {
 
 // welcome message and directions
 
-console.log(
+alert(
 `Welcome to Tic Tac Toe!
-\nObject of the game: To win, get three Xs or Os in a row, column or diagonal.
+\nObject of the game: To win, get three Xs or Os in a row, column, or diagonal.
 \nGame play: 1. Decide who is X and who is O. 2. Take turns typing the row and colunn codes of your selected spot.
-\nFor example, the code for the top-left is tl, for the mid-middle spot it is mm,
-\nand for the lower-right spot, the code is lr.`
+\nFor example, the code for the top-left is tl, for the mid-middle spot it is mm, and for the lower-right spot, the code is lr.`
 );
 
 // printed board
 
 printBoard = (board) =>
 
-    console.log(
+    alert(
         `${Object.values(board)[0]} ${Object.values(board)[1]} ${Object.values(board)[2]}
-        \n ${Object.values(board)[3]} ${Object.values(board)[4]} ${Object.values(board)[5]}
-        \n ${Object.values(board)[6]} ${Object.values(board)[7]} ${Object.values(board)[8]}`
+        \n${Object.values(board)[3]} ${Object.values(board)[4]} ${Object.values(board)[5]}
+        \n${Object.values(board)[6]} ${Object.values(board)[7]} ${Object.values(board)[8]}`
     );
 
-printBoard(theBoard);
 //win announcement and game exit
 
-
+winMessage = () => {
+    printBoard(theBoard);
+    alert(`${turn} wins!`);
+    exit();
+}
 
 // players enter moves
 
+    let turn = 'X';
+
+    for (let i = 0; i < 9; i++) {
+        printBoard(theBoard);
+        move = prompt(`${turn}, it is your turn. Select a spot.`);
+        theBoard[move] = `${turn}`;
+        
+        //determine and announce winner
+
+
+        if (turn === 'X') {
+            turn = 'O';
+        } else {
+            turn = 'X';
+        }
+    }
+printBoard(theBoard);
+alert(`It is a draw!
+\n `);
 
 
 
-//determine and announce winner
+
