e07526ccde8e7ee1fb294fec4d62217e45c866ec
Faraja17 <farajakthompson@gmail.com>
2022-06-13 11:44:09 -0400

    set up an object for the board and wrote welcome
    It took me a while to get the line break \n syntax, and I am still unsure about this string because in code pen, it is printed in double quotes and some of the words and characters are different colors.
    
    I don't think it likes !, Game, For, and 1. and 2. within a string.

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,9 +1,16 @@
 // Tic Tac Toe Grid
-
+const theBoard = {
+    tl: '-', tm: '-', tr: '-',
+    ml: '-', mm: '-', mr: '-',
+    ll: '-', lm: '-', lr: '-'
+}
 
 // welcome message and directions
-
-
+console.log("Welcome to Tic Tac Toe!
+\nObject of the game: To win, get three Xs or Os in a row, column or diagonal.
+\nGame play: 1. Decide who is X and who is O. 2. Take turns typing the row and colunn codes of your selected spot.
+\nFor example, the code for the top-left is tl, for the mid-middle spot it is mm,
+\nand for the lower-right spot, the code is lr."");
 
 // printed board
 
