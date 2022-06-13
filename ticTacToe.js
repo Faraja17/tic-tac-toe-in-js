0ada3a927f64b363b01cc1ce3d647b194b62c5fc
Faraja Thompson <farajakthompson@gmail.com>
2022-06-12 22:03:18 -0400

    I have written an outline of my plan in comments.

---
+++ ticTacToe.js
@@ -0,0 +1,21 @@
+// Tic Tac Toe Grid
+
+
+// welcome message and directions
+
+
+
+// printed board
+
+
+
+//win announcement and game exit
+
+
+
+// players enter moves
+
+
+
+
+//determine and announce winner
